90dc8e384a762a45ff42d6f8cd18f1eb8fd95e77
robsonSchuster <68903975+robsonSchuster@users.noreply.github.com>
2025-02-20 11:16:05 -0300

    Update settings.js

--- src/settings.js
+++ src/settings.js
@@ -1,1 +1,15 @@
-
+module.exports = {
+  uiPort: process.env.PORT || 1880,
+  flowFile: "/opt/render/project/src/flows/flows.json",
+  userDir: "/opt/render/project/src/flows",
+  credentialSecret: "suaSenhaSegura",
+  httpStatic: "/opt/render/project/src/flows", // 👈 Pasta exposta
+  httpAdminRoot: "/admin", // Evita conflito com a rota estática
+  logging: {
+    console: {
+      level: "info",
+      metrics: false,
+      audit: false,
+    },
+  },
+};
